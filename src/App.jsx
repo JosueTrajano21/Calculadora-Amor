f52819ab8843c5cb8e3f5629138a80057ba503e1
JosueTrajano21 <josuedev.trajano@gmail.com>
2025-12-02 14:10:23 -0300

    Finalização Projeto

--- src/App.jsx
+++ src/App.jsx
@@ -79,10 +79,10 @@ function App() {
                 <div className="bg-pink-300 flex flex-col gap-2 p-2 rounded-2xl">
                   
                   <div className="flex items-center break-all">
-                  <img src={iconPerfil} alt="Icone" className="w-4 h-4 mr-2"/> {item.primeiroNome}
+                  <img src={iconPerfil} alt="Icone" className="w-4 h-4 mr-2"/> {item.firstName}
                   </div>
                   <div className="flex items-center break-all">
-                  <img src={iconPerfil} alt="Icone" className="w-4 h-4 mr-2"/> {item.segundoNome} 
+                  <img src={iconPerfil} alt="Icone" className="w-4 h-4 mr-2"/> {item.secondName} 
                   </div>
                   <div className="">
                   {item.percentage}%  de compatibilidade 💕
@@ -118,12 +118,12 @@ function App() {
 
           {/* Caso carregando seja TRUE vai aparecer */}
           {carregando ? (
-            <button type="button" class="flex items-center px-4 py-2 bg-pink-400 text-white rounded-[7px]" xmlns="http://www.w3.org/2000/svg" disabled>  
-              <svg class="mr-3 size-5 animate-spin" viewBox="0 0 24 24">
-                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            <button type="button" className="flex items-center px-4 py-2 bg-pink-400 text-white rounded-[7px]" xmlns="http://www.w3.org/2000/svg" disabled>  
+              <svg className="mr-3 size-5 animate-spin" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
               </svg>
-              <span class="font-medium">Carregando</span>
+              <span className="font-medium">Carregando</span>
             </button>
           ) : (
             <button 
@@ -143,10 +143,10 @@ function App() {
             </p>
           )}
 
-          {/* Caso resultado seja TRUE vai aparecer -> Null é um tipo false*/}
+
           {resultado && (
           <div className="flex flex-col gap-4 bg-pink-100 p-6 rounded-[20px] max-w-[300px] border-2 border-pink-200">
-            <h1 className="font-bold text-[26px] break-all">{resultado.primeiroNome} ❤️ {resultado.segundoNome}</h1>
+            <h1 className="font-bold text-[26px] break-all">{resultado.firstName} ❤️ {resultado.secondName}</h1>
             <p className="font-bold text-pink-500 text-[20px] pb-3">{resultado.percentage}% de compatibilidade</p>
             <p >Mensagem</p>
             <p>{resultado.result}</p>
